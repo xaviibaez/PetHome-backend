9b711dbc7716ef141dadc0f3817f0b45c44e3e7f
Xavi Ibáñez Martínez <241xavi@gmail.com>
2022-01-09 23:04:07 +0100

    Create PetRepository.java

---
+++ src/main/java/com/pethome/pethome/repository/PetRepository.java
@@ -0,0 +1,14 @@
+package com.pethome.pethome.repository;
+
+import java.util.Optional;
+
+import com.pethome.pethome.models.Pet;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PetRepository extends JpaRepository<Pet, Long> {
+    Optional<Pet> findByName(String username);
+    Boolean existsByName(String username);
+}
