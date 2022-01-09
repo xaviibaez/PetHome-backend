945befb9326bb2ad362cc095305cddea0f232e64
Xavi Ibáñez Martínez <241xavi@gmail.com>
2022-01-09 12:06:21 +0100

    Create Constants.java

---
+++ src/main/java/com/pethome/pethome/constants/Constants.java
@@ -0,0 +1,6 @@
+package com.pethome.pethome.constants;
+
+public final class Constants {
+    public static final String URL_LOCALHOST = "http://localhost:3000";
+    public static final String URL_REMOTE = "https://pethome-frontend.herokuapp.com/";
+}
