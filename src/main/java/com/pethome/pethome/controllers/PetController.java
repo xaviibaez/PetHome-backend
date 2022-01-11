4eef68847a2216b97e0cafbc4a5c63a71735c3fd
Xavi Ibáñez Martínez <241xavi@gmail.com>
2022-01-12 00:41:41 +0100

    testing

--- src/main/java/com/pethome/pethome/controllers/PetController.java
+++ src/main/java/com/pethome/pethome/controllers/PetController.java
@@ -11,6 +11,7 @@ import com.pethome.pethome.exception.ResourceDuplicateException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1/")
 public class PetController implements IPetController{
