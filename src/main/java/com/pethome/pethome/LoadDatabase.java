1bec67d3f6e383ab3aa7e6697fc6352f68f1c720
Xavi Ibáñez Martínez <241xavi@gmail.com>
2022-07-24 01:43:33 +0200

    Update LoadDatabase.java

--- src/main/java/com/pethome/pethome/LoadDatabase.java
+++ src/main/java/com/pethome/pethome/LoadDatabase.java
@@ -1,16 +1,13 @@
-/*package com.pethome.pethome;
+package com.pethome.pethome;
 
 import com.pethome.pethome.models.Role;
 import com.pethome.pethome.models.TypePet;
 import com.pethome.pethome.models.ERole;
-import com.pethome.pethome.models.Pet;
-import com.pethome.pethome.repository.PetRepository;
-import com.pethome.pethome.repository.RoleRepository;
-import com.pethome.pethome.repository.TypePetRepository;
+import com.pethome.pethome.repository.IRoleRepository;
+import com.pethome.pethome.repository.ITypePetRepository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,34 +17,33 @@ public class LoadDatabase {
 
   private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
   
-  @Autowired
-	private TypePetRepository typePetRepository;
-
   @Bean
-  CommandLineRunner initDatabaseRole(RoleRepository roleRepository) {
+  CommandLineRunner initDatabaseRole(IRoleRepository roleRepository) {
+    if(!roleRepository.findAll().isEmpty()){
+      return args -> {
+        log.info("No roles to insert");
+      };    
+    }
+
     return args -> {
-      if(!roleRepository.existsByName((long) 1)){
-        log.info("Preloading " + roleRepository.save(new Role(ERole.ROLE_USER)));
-        log.info("Preloading " + roleRepository.save(new Role(ERole.ROLE_MODERATOR)));
-        log.info("Preloading " + roleRepository.save(new Role(ERole.ROLE_ADMIN)));
-      }
+      log.info("Preloading " + roleRepository.save(new Role(ERole.ROLE_USER)));
+      log.info("Preloading " + roleRepository.save(new Role(ERole.ROLE_MODERATOR)));
+      log.info("Preloading " + roleRepository.save(new Role(ERole.ROLE_ADMIN)));
     };
   }
 
   @Bean
-  CommandLineRunner initDatabaseTypePet(TypePetRepository typePetRepository) {
+  CommandLineRunner initDatabaseTypePet(ITypePetRepository typePetRepository) {
+    if(!typePetRepository.findAll().isEmpty()){
+      return args -> {
+        log.info("No type pets to insert");
+      };    
+    }
+
     return args -> {
       log.info("Preloading " + typePetRepository.save(new TypePet("Dog")));
       log.info("Preloading " + typePetRepository.save(new TypePet("Cat")));
-    };
-  }
-
-  @Bean
-  CommandLineRunner initDatabasePet(PetRepository petRepository) {
-    return args -> {
-      log.info("Preloading " + petRepository.save(new Pet("Perrito", "Descripcion Perro", "Comportamiento Perro", 1, true, typePetRepository.findByName("Dog").get())));
-      log.info("Preloading " + petRepository.save(new Pet("Gatito", "Descripcion Gato", "Comportamiento Gato", 2, false, typePetRepository.findByName("Cat").get())));
+      log.info("Preloading " + typePetRepository.save(new TypePet("Fish")));
     };
   }
 }
-*/
