60fcf8107da85745543c676aea6efd806e79386a
Xavi Ibáñez Martínez <241xavi@gmail.com>
2022-07-26 20:15:27 +0200

    Create PetModelAssembler.java

---
+++ src/main/java/com/pethome/pethome/assembler/PetModelAssembler.java
@@ -0,0 +1,23 @@
+package com.pethome.pethome.assembler;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import com.pethome.pethome.controllers.PetController;
+import com.pethome.pethome.models.Pet;
+
+@Component
+public class PetModelAssembler implements RepresentationModelAssembler<Pet, EntityModel<Pet>> {
+
+    @Override
+    public EntityModel<Pet> toModel(Pet pet) {
+    
+        return EntityModel.of(pet, //
+            linkTo(methodOn(PetController.class).getPetById(pet.getId())).withSelfRel(),
+            linkTo(methodOn(PetController.class).getAllPets()).withRel("employees"));
+    }
+}
+
