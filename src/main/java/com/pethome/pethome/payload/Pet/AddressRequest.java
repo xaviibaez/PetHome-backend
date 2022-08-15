00a570d110c94d8ae4199651ac92e0f4eee64ace
Xavi Ibáñez Martínez <241xavi@gmail.com>
2022-08-16 00:40:59 +0200

    Create AddressRequest.java

---
+++ src/main/java/com/pethome/pethome/payload/Pet/AddressRequest.java
@@ -0,0 +1,43 @@
+package com.pethome.pethome.payload.Pet;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class AddressRequest {
+    @NotBlank
+    @Size(max = 20)
+  	private String name;
+
+	@NotBlank
+    @Size(max = 50)
+	private String description;
+
+    @NotBlank
+	private Long locationId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(Long locationId) {
+        this.locationId = locationId;
+    }
+
+    
+}
